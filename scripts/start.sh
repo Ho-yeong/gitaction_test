d41dca961acae363730902c6ad43710fcc0cbc03
simon <ghdud0503@gmail.com>
2024-01-15 13:02:08 +0900

    script fixed

--- scripts/start.sh
+++ scripts/start.sh
@@ -7,7 +7,7 @@ TIME_NOW=$(date +%c)
 
 cd $PROJECT_ROOT
 
-pm2 delete $APP_NAME
-pm2 start npm --name $APP_NAME -- start
+/home/ubuntu/.nvm/versions/node/v21.5.0/bin/pm2 delete $APP_NAME
+/home/ubuntu/.nvm/versions/node/v21.5.0/bin/pm2 start npm --name $APP_NAME -- start
 
 echo "$TIME_NOW > Deploy has been completed"
